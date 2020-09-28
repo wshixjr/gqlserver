5eec84eb8ac2d9b2f6859bbbe54d20153f232dfa
wangs <shi.wang@broadlink.com.cn>
2020-09-28 17:56:47 +0800

    增加对echo的支持
    增加对echo的支持

--- go.mod
+++ go.mod
@@ -4,5 +4,11 @@ go 1.15
 
 require (
 	github.com/99designs/gqlgen v0.13.0
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/valyala/fasttemplate v1.2.1 // indirect
 	github.com/vektah/gqlparser/v2 v2.1.0
+	gorm.io/driver/mysql v1.0.1
+	gorm.io/gorm v1.20.2
 )
--- server.go
+++ server.go
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-09-27 18:28:28
- * @LastEditTime: 2020-09-28 10:11:52
+ * @LastEditTime: 2020-09-28 17:55:45
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \gqlgentoto\server.go
@@ -9,9 +9,10 @@
 package main
 
 import (
-	"log"
-	"net/http"
 	"os"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
@@ -19,7 +20,7 @@ import (
 	"github.com/testgql/gqlgen-todos/graph/generated"
 )
 
-const defaultPort = "8787"
+const defaultPort = "8686"
 
 func main() {
 	port := os.Getenv("PORT")
@@ -27,11 +28,26 @@ func main() {
 		port = defaultPort
 	}
 
+	e := echo.New()
+	//e.SetBinder(&binding.EchoBinder{})
+
+	// Middleware
+	e.Use(middleware.CORS())
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	// e.Use(middleware.BodyDump(func(c echo.Context, reqBody, resBody []byte) {
+	// }))
+
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	// http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	// http.Handle("/query", srv)
 
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	// log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	// log.Fatal(http.ListenAndServe(":"+port, nil))
+
+	e.Any("/", echo.WrapHandler(playground.Handler("GraphQL playground", "/query")))
+	e.POST("/query", echo.WrapHandler(srv))
+
+	e.Start(":" + defaultPort)
 }
